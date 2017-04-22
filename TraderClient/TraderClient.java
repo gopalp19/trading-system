c95d2b5effeb86103a255ccf10efe501ef922e73
acham1 <alanchamis@gmail.com>
2017-04-22 15:56:37 -0500

    Added a TraderClient stub, but this is mainly just a push exercise for using GitHub.

---
+++ TraderClient/TraderClient.java
@@ -0,0 +1,15 @@
+package TraderClient;
+
+/**
+ * TraderClient, a client process that: (1) initiates client-server interaction, (2)...
+ * @author Alan
+ */
+public class TraderClient {
+    /**
+     * main driver function for TraderClient process.
+     * @param args - command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello I am a TraderClient");
+    }
+}

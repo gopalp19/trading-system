cd16dd04f13148ecbcda30b7de802074734ff29f
acham1 <alanchamis@gmail.com>
2017-05-08 01:58:32 -0500

    Added message support for Buy, Sell, BuyResult, SellResult

---
+++ Messenger/Message.java
@@ -0,0 +1,11 @@
+package Messenger;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * Created by Alan on 5/7/2017.
+ */
+public abstract class Message {
+    abstract ArrayList<String> toStringList();
+}

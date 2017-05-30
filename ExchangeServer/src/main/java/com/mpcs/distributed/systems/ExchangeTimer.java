d9225017c579718c1b783fb013dc2667d3760c0d
acham1 <alanchamis@gmail.com>
2017-05-30 00:42:46 -0500

    Speed up clock 2x to 2.5 real seconds per fake hour

--- ExchangeServer/src/main/java/com/mpcs/distributed/systems/ExchangeTimer.java
+++ ExchangeServer/src/main/java/com/mpcs/distributed/systems/ExchangeTimer.java
@@ -10,7 +10,7 @@ import java.util.*;
  * @author Alan
  */
 public class ExchangeTimer extends TimerTask {
-	private static int period = 5000; // real ms between simulated hours
+	private static int period = 2500; // real ms between simulated hours
 	private static LocalDateTime zeroTime = LocalDateTime.of(2016, 1, 4, 8, 0);
 	
 	private AtomicInteger ticks = new AtomicInteger(0);

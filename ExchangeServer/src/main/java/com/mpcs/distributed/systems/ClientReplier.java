7f42eb10a2497923773a9603c298dedc9688c170
acham1 <alanchamis@gmail.com>
2017-05-29 22:25:05 -0500

    Fixed exchange logging name

--- ExchangeServer/src/main/java/com/mpcs/distributed/systems/ClientReplier.java
+++ ExchangeServer/src/main/java/com/mpcs/distributed/systems/ClientReplier.java
@@ -23,7 +23,7 @@ public class ClientReplier implements Runnable {
 	
 	public ClientReplier() {
 		try {
-			logger = new PrintWriter(new File(ExchangeServer.exchange.toString() + "." + ".log"));			
+			logger = new PrintWriter(new File(ExchangeServer.exchange.toString() + "." + System.currentTimeMillis() + ".log"));			
 		} catch (IOException e) {
 			logger = null;
 			System.out.println("Error: IOException when trying to start a log file for " + ExchangeServer.exchange);

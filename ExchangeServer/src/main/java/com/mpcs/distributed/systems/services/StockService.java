7d09047ac6ce2a13fab9efd8114351160e31d907
acham1 <alanchamis@gmail.com>
2017-05-31 16:55:03 -0500

    debug attempt stock drift

--- ExchangeServer/src/main/java/com/mpcs/distributed/systems/services/StockService.java
+++ ExchangeServer/src/main/java/com/mpcs/distributed/systems/services/StockService.java
@@ -29,86 +29,90 @@ public class StockService {
     private StockQuantityRepository stockQuantityRepository;
 
 	public void buyStock(BuyMessage message, BuyResultMessage br) {
-		System.out.println("Inside stock service buyStock");
-	
-		//LocalDateTime currentExchangeTime =  ExchangeServer.exchangeTimer.getTime().truncatedTo(ChronoUnit.HOURS);
-		LocalDateTime currentExchangeTime = marketHours(ExchangeServer.exchangeTimer.getTime());
-		
-		List<StockPrice> stockPrices = stockPriceRepository.findByStockNameAndExchangeNameAndSystemDateTime(message.stock.toString(), ExchangeServer.exchange.toString(), currentExchangeTime);
-		
-		if(stockPrices.isEmpty()){
-			//Stock not in exchanges region
-			System.out.println("Stock not in exchange's region");
-		}else{
-			//Stock exists 
-			List<StockQuantity> stockQuantityList = stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), ExchangeServer.exchange.toString());
+		synchronized (StockService.class) {
+			System.out.println("Inside stock service buyStock");
 			
-			if(!stockQuantityList.isEmpty()){
-
-				StockQuantity stockQuantity = stockQuantityList.get(0);
-				StockPrice stockPrice = stockPrices.get(0);
-				
-				if(stockQuantity.getIpoTime().isAfter(currentExchangeTime)){
-					//IPO has not started and stocks do not exist
-					br.notificationMessage = "IPO has not started, no stock exists!";
-					return;
-				}
-				
-				//Make sure amount of stocks in db is greater than how many user wants
-				if(stockQuantity.getQuantity() - getThreshold(message.stock) >= message.quantity){
-					//Update db to remove stocks
-					int updatedStockQuantity = stockQuantity.getQuantity() - message.quantity;
-					stockQuantity.setQuantity(updatedStockQuantity);
-					stockQuantityRepository.save(stockQuantity);
-					
-        			br.quantityBought = message.quantity;
-        			br.totalPrice = stockPrice.getPrice() * message.quantity;
-        			System.out.println("new qty of " + message.stock + " is " + updatedStockQuantity);
-        			ExchangeServer.logger.messageQueue.add(br);
-					//Return success, user bough stocks
-				}else{
-					//Quantity too large...give error message
-					br.notificationMessage = "Quantity demanded is greater than current stock supply!";
-				}
+			//LocalDateTime currentExchangeTime =  ExchangeServer.exchangeTimer.getTime().truncatedTo(ChronoUnit.HOURS);
+			LocalDateTime currentExchangeTime = marketHours(ExchangeServer.exchangeTimer.getTime());
+			
+			List<StockPrice> stockPrices = stockPriceRepository.findByStockNameAndExchangeNameAndSystemDateTime(message.stock.toString(), ExchangeServer.exchange.toString(), currentExchangeTime);
+			
+			if(stockPrices.isEmpty()){
+				//Stock not in exchanges region
+				System.out.println("Stock not in exchange's region");
 			}else{
-				//Stock is not found in quantities table...this should never happen at this point
-				System.out.println("Stock not found in qty table");
+				//Stock exists 
+				List<StockQuantity> stockQuantityList = stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), ExchangeServer.exchange.toString());
+				
+				if(!stockQuantityList.isEmpty()){
+
+					StockQuantity stockQuantity = stockQuantityList.get(0);
+					StockPrice stockPrice = stockPrices.get(0);
+					
+					if(stockQuantity.getIpoTime().isAfter(currentExchangeTime)){
+						//IPO has not started and stocks do not exist
+						br.notificationMessage = "IPO has not started, no stock exists!";
+						return;
+					}
+					
+					//Make sure amount of stocks in db is greater than how many user wants
+					if(stockQuantity.getQuantity() - getThreshold(message.stock) >= message.quantity){
+						//Update db to remove stocks
+						int updatedStockQuantity = stockQuantity.getQuantity() - message.quantity;
+						stockQuantity.setQuantity(updatedStockQuantity);
+						stockQuantityRepository.save(stockQuantity);
+						
+	        			br.quantityBought = message.quantity;
+	        			br.totalPrice = stockPrice.getPrice() * message.quantity;
+	        			System.out.println("new qty of " + message.stock + " is " + updatedStockQuantity);
+	        			ExchangeServer.logger.messageQueue.add(br);
+						//Return success, user bough stocks
+					}else{
+						//Quantity too large...give error message
+						br.notificationMessage = "Quantity demanded is greater than current stock supply!";
+					}
+				}else{
+					//Stock is not found in quantities table...this should never happen at this point
+					System.out.println("Stock not found in qty table");
+				}
 			}
 		}
 	}
 
 	public void sellStock(SellMessage message, SellResultMessage sr) {
-		System.out.println("Inside stock service sellStock");
-		LocalDateTime currentExchangeTime = marketHours(ExchangeServer.exchangeTimer.getTime());
-		
-		List<StockPrice> stockPrices = stockPriceRepository.findByStockNameAndExchangeNameAndSystemDateTime(message.stock.toString(), ExchangeServer.exchange.toString(), currentExchangeTime);
-		
-		if(stockPrices.isEmpty()){
-			//Stock not in exchanges region
-			System.out.println("Stock not in exchange's region");
-		} else {
-			//Stock exists 
-			List<StockQuantity> stockQuantityList = stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), ExchangeServer.exchange.toString());
+		synchronized(StockService.class) {
+			System.out.println("Inside stock service sellStock");
+			LocalDateTime currentExchangeTime = marketHours(ExchangeServer.exchangeTimer.getTime());
 			
-			if(!stockQuantityList.isEmpty()){
+			List<StockPrice> stockPrices = stockPriceRepository.findByStockNameAndExchangeNameAndSystemDateTime(message.stock.toString(), ExchangeServer.exchange.toString(), currentExchangeTime);
+			
+			if(stockPrices.isEmpty()){
+				//Stock not in exchanges region
+				System.out.println("Stock not in exchange's region");
+			} else {
+				//Stock exists 
+				List<StockQuantity> stockQuantityList = stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), ExchangeServer.exchange.toString());
+				
+				if(!stockQuantityList.isEmpty()){
 
-				StockQuantity stockQuantity = stockQuantityList.get(0);
-				StockPrice stockPrice= stockPrices.get(0);
-				
-				//Update db to insert stocks
-				int updatedStockQuantity = stockQuantity.getQuantity() + message.quantity;
-				stockQuantity.setQuantity(updatedStockQuantity);
-				stockQuantityRepository.save(stockQuantity);
-				
-    			sr.quantitySold = message.quantity;
-    			sr.totalPrice = stockPrice.getPrice() * message.quantity;
-    			System.out.println("new qty of " + message.stock + " is " + updatedStockQuantity);
-    			ExchangeServer.logger.messageQueue.add(sr);
+					StockQuantity stockQuantity = stockQuantityList.get(0);
+					StockPrice stockPrice= stockPrices.get(0);
 					
-    			//Return success, user sold stocks
-			}else{
-				//Stock is not found in quantities table...this should never happen at this point
-				System.out.println("Stock not found in qty table");
+					//Update db to insert stocks
+					int updatedStockQuantity = stockQuantity.getQuantity() + message.quantity;
+					stockQuantity.setQuantity(updatedStockQuantity);
+					stockQuantityRepository.save(stockQuantity);
+					
+	    			sr.quantitySold = message.quantity;
+	    			sr.totalPrice = stockPrice.getPrice() * message.quantity;
+	    			System.out.println("new qty of " + message.stock + " is " + updatedStockQuantity);
+	    			ExchangeServer.logger.messageQueue.add(sr);
+						
+	    			//Return success, user sold stocks
+				}else{
+					//Stock is not found in quantities table...this should never happen at this point
+					System.out.println("Stock not found in qty table");
+				}
 			}
 		}
 	}
@@ -150,9 +154,11 @@ public class StockService {
 	 * unreserve some stock
 	 */
 	public void unreserveStock(Stock stock, int quantity) {
-		System.out.println("Inside stock service unreserveStock");
-		int old = getThreshold(stock);
-		thresholds.put(stock,  old - quantity);
+		synchronized (StockService.class) {
+			System.out.println("Inside stock service unreserveStock");
+			int old = getThreshold(stock);
+			thresholds.put(stock,  old - quantity);			
+		}
 	}
 	
 	/**

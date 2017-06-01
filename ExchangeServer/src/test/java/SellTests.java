5d9ec5e5e9420e2a5f6e97c980cfdf066cb04be5
Pradeep Gopal <ggopalp19@gmail.com>
2017-06-01 13:48:43 -0500

    Added sell test and updated buy test

--- ExchangeServer/src/test/java/SellTests.java
+++ ExchangeServer/src/test/java/SellTests.java
@@ -23,13 +23,13 @@ import com.mpcs.distributed.systems.repositories.StockPriceRepository;
 import com.mpcs.distributed.systems.repositories.StockQuantityRepository;
 import com.mpcs.distributed.systems.services.StockService;
 
-import messenger.BuyMessage;
-import messenger.BuyResultMessage;
+import messenger.SellMessage;
+import messenger.SellResultMessage;
 import resourcesupport.Exchange;
 import resourcesupport.Stock;
 
 @RunWith(MockitoJUnitRunner.class)
-public class BuyTests {
+public class SellTests {
 	
 	@InjectMocks
 	StockService stockService;
@@ -40,8 +40,8 @@ public class BuyTests {
 	@Mock
 	private StockQuantityRepository stockQuantityRepository;
 	
-	BuyMessage message;
-	BuyResultMessage result;
+	SellMessage message;
+	SellResultMessage result;
 	
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
@@ -52,7 +52,7 @@ public class BuyTests {
         MockitoAnnotations.initMocks(this);
 		setupMessages();
     }
- 
+	 
 	@Test
 	public void wrongExchangetest() {
 		
@@ -64,9 +64,9 @@ public class BuyTests {
 		when(stockPriceRepository.findByStockNameAndExchangeNameAndSystemDateTime("IBM", 
 				"NEW_YORK_STOCK_EXCHANGE", time)).thenReturn(stockPrices);
 		
-		stockService.buyStock(message, result);
+		stockService.sellStock(message, result);
 		
-		assertEquals(result.quantityBought, (Integer) 0);
+		assertEquals(result.quantitySold, (Integer) 0);
 	}
 	
 	@Test
@@ -86,61 +86,9 @@ public class BuyTests {
 		when(stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), 
 				ExchangeServer.exchange.toString())).thenReturn(stockQuantities);
 		
-		stockService.buyStock(message, result);
+		stockService.sellStock(message, result);
 		
-		assertEquals(result.quantityBought, (Integer) 0);
-	}
-	
-	@Test
-	public void futureIPOtest() {
-		
-		ExchangeServer.exchange = Exchange.NEW_YORK_STOCK_EXCHANGE;
-		
-		List<StockPrice> stockPrices = new ArrayList<>();
-		List<StockQuantity> stockQuantities = new ArrayList<>();
-		LocalDateTime time = LocalDateTime.of(2016, Month.JANUARY, 1, 9, 0);
-		LocalDateTime laterIPOTime = LocalDateTime.of(2016, Month.JANUARY, 2, 9, 0);
-		StockPrice price = new StockPrice("IBM", "NEW_YORK_STOCK_EXCHANGE", (float) 112.50, time);
-		StockQuantity quantity = new StockQuantity("IBM", "NEW_YORK_STOCK_EXCHANGE", 500, laterIPOTime);
-		stockPrices.add(price);
-		stockQuantities.add(quantity);
-		
-		when(stockPriceRepository.findByStockNameAndExchangeNameAndSystemDateTime("IBM", 
-				"NEW_YORK_STOCK_EXCHANGE", time)).thenReturn(stockPrices);
-		
-		when(stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), 
-				ExchangeServer.exchange.toString())).thenReturn(stockQuantities);
-		
-		stockService.buyStock(message, result);
-		
-		assertEquals(result.quantityBought, (Integer) 0);
-		assertEquals(result.notificationMessage, "IPO has not started, no stock exists!");
-	}
-	
-	@Test
-	public void lowStockSupplytest() {
-		
-		ExchangeServer.exchange = Exchange.NEW_YORK_STOCK_EXCHANGE;
-		
-		List<StockPrice> stockPrices = new ArrayList<>();
-		List<StockQuantity> stockQuantities = new ArrayList<>();
-		LocalDateTime time = LocalDateTime.of(2016, Month.JANUARY, 1, 9, 0);
-		StockPrice price = new StockPrice("IBM", "NEW_YORK_STOCK_EXCHANGE", (float) 112.50, time);
-		StockQuantity quantity = new StockQuantity("IBM", "NEW_YORK_STOCK_EXCHANGE", 500, time);
-		stockPrices.add(price);
-		stockQuantities.add(quantity);
-		
-		when(stockPriceRepository.findByStockNameAndExchangeNameAndSystemDateTime("IBM", 
-				"NEW_YORK_STOCK_EXCHANGE", time)).thenReturn(stockPrices);
-		
-		when(stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), 
-				ExchangeServer.exchange.toString())).thenReturn(stockQuantities);
-		
-		message.quantity = 1000;
-		stockService.buyStock(message, result);
-		
-		assertEquals(result.quantityBought, (Integer) 0);
-		assertEquals(result.notificationMessage, "Quantity demanded is greater than current stock supply!");
+		assertEquals(result.quantitySold, (Integer) 0);
 	}
 	
 	@Test
@@ -163,14 +111,14 @@ public class BuyTests {
 		when(stockQuantityRepository.findByStockNameAndExchangeName(message.stock.toString(), 
 				ExchangeServer.exchange.toString())).thenReturn(stockQuantities);
 		
-		stockService.buyStock(message, result);
+		stockService.sellStock(message, result);
 		
-		assertEquals(result.quantityBought, message.quantity);
-	}
-	
-	public void setupMessages(){
-		message = new BuyMessage(Exchange.NEW_YORK_STOCK_EXCHANGE, "test", Stock.IBM, 10, null, null);
-		result = new BuyResultMessage(Exchange.NEW_YORK_STOCK_EXCHANGE, "test", Stock.IBM, (Integer) 0, (float) 0, null);
+		assertEquals(result.quantitySold, message.quantity);
 	}
 
+
+	public void setupMessages(){
+		message = new SellMessage(Exchange.NEW_YORK_STOCK_EXCHANGE, "test", Stock.IBM, 10, null, null);
+		result = new SellResultMessage(Exchange.NEW_YORK_STOCK_EXCHANGE, "test", Stock.IBM, (Integer) 0, (float) 0, null);
+	}
 }
